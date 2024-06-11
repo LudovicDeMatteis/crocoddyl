c1e434d425640c597af3317b95db26f83bb38e89
Ludovic De Matteis <ldematteis@laas.fr>
2024-06-11 10:15:31 +0200

    Add contact 6D calc and calcDiff

---
+++ include/crocoddyl/multibody/contacts/contact-6d-loop.hxx
@@ -0,0 +1,168 @@
+
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2019-2023, LAAS-CNRS, University of Edinburgh,
+//                          Heriot-Watt University
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/algorithm/kinematics-derivatives.hpp>
+
+#include "crocoddyl/core/utils/exception.hpp"
+#include "crocoddyl/multibody/contacts/contact-6d.hpp"
+
+namespace crocoddyl {
+
+template <typename Scalar>
+ContactModel6DLoopTpl<Scalar>::ContactModel6DLoopTpl(
+    boost::shared_ptr<StateMultibody> state, const pinocchio::FrameIndex frame1_id,
+    const pinocchio::FrameIndex frame2_id, const pinocchio::ReferenceFrame ref, 
+    const std::size_t nu, const Vector2s& gains)
+    : Base(state, pinocchio::ReferenceFrame::LOCAL, 6, nu), gains_(gains), 
+    frame1_id_(frame1_id), frame2_id_(frame2_id), is_frame_(true) {
+  if(ref != pinocchio::ReferenceFrame::LOCAL){
+    std::cerr << "Warning: Only reference frame LOCAL is supported for 6D loop contacts\n";
+  }
+}
+
+template <typename Scalar>
+ContactModel6DLoopTpl<Scalar>::ContactModel6DLoopTpl(
+    boost::shared_ptr<StateMultibody> state, const pinocchio::FrameIndex frame1_id,
+    const pinocchio::FrameIndex frame2_id, const pinocchio::ReferenceFrame ref, 
+    const Vector2s& gains)
+    : Base(state, pinocchio::ReferenceFrame::LOCAL, 6), gains_(gains),
+    joint1_id_(frame1_id), frame2_id_(frame2_id), is_frame_(true) {
+  if(ref != pinocchio::ReferenceFrame::LOCAL){
+    std::cerr << "Warning: Only reference frame LOCAL is supported for 6D loop contacts\n";
+  }
+}
+
+template <typename Scalar>
+ContactModel6DLoopTpl<Scalar>::ContactModel6DLoopTpl(
+    boost::shared_ptr<StateMultibody> state, 
+    const int joint1_id, const SE3& joint1_placement,
+    const int joint2_id, const SE3& joint2_placement,
+    const pinocchio::ReferenceFrame ref, const std::size_t nu, const Vector2s& gains)
+    : Base(state, pinocchio::ReferenceFrame::LOCAL, 6, nu),
+      gains_(gains), joint1_id_(joint1_id), joint2_id_(joint2_id),
+      joint1_placement_(joint1_placement), joint2_placement_(joint2_placement), is_frame_(false) {
+  std::cout << "Defining contact closed loop from joints\n" << std::endl;
+}
+
+template <typename Scalar>
+ContactModel6DLoopTpl<Scalar>::~ContactModel6DLoopTpl() {}
+
+template <typename Scalar>
+void ContactModel6DLoopTpl<Scalar>::calc(
+    const boost::shared_ptr<ContactDataAbstract>& data,
+    const Eigen::Ref<const VectorXs>&) {
+  Data* d = static_cast<Data*>(data.get());
+  pinocchio::updateFramePlacements<Scalar>(*state_->get_pinocchio().get(),
+                                          *d->pinocchio);
+  std::cout << "Calculating contact 6D loop\n" << std::endl;
+  d->j1Xf1 = joint1_placement_.toActionMatrix();
+  d->j2Xf2 = joint2_placement_.toActionMatrix();  
+
+  std::cout << "Calculating contact Jacobian 6D loop\n" << std::endl;
+  pinocchio::getJointJacobian(*state_->get_pinocchio().get(), *d->pinocchio,
+                            joint1_id_, pinocchio::LOCAL, d->j1Jj1);
+  pinocchio::getJointJacobian(*state_->get_pinocchio().get(), *d->pinocchio,
+                            joint2_id_, pinocchio::LOCAL, d->j2Jj2);
+  std::cout << "Calculating contact Jacobian transforms 6D loop\n" << std::endl;
+  d->f1Jf1 = d->j1Xf1.inverse() * d->j1Jj1;
+  d->f2Jf2 = d->j2Xf2.inverse() * d->j2Jj2;
+
+  SE3 oMf1 = d->pinocchio->oMi[joint1_id_].act(joint1_placement_);
+  SE3 oMf2 = d->pinocchio->oMi[joint2_id_].act(joint2_placement_);
+  d->f1Mf2 = oMf1.actInv(oMf2);
+  d->f1Xf2 = d->f1Mf2.toActionMatrix();
+
+  d->Jc = d->f1Jf1 - d->f1Xf2 * d->f2Jf2;
+  // Compute the acceleration drift
+  std::cout << "Calculating contact acceleration drift 6D loop\n" << std::endl;
+  if(joint1_id_ > 0){
+    d->f1vf1 = joint1_placement_.actInv(d->pinocchio->v[joint1_id_]);
+    d->f1af1 = joint1_placement_.actInv(d->pinocchio->a[joint1_id_]);
+  }
+  else{
+    d->f1vf1.setZero();
+    d->f1af1.setZero();
+  }
+  if(joint2_id_ > 0){
+    d->f2vf2 = joint2_placement_.actInv(d->pinocchio->v[joint2_id_]);
+    d->f2af2 = joint2_placement_.actInv(d->pinocchio->a[joint2_id_]);
+  }
+  else{
+    d->f2vf2.setZero();
+    d->f2af2.setZero();
+  }
+  std::cout << "Calculated contact acceleration drift 6D loop\n" << std::endl;
+  d->f1vf2 = d->f1Mf2.act(d->f2vf2);
+  d->f1af2 = d->f1Mf2.act(d->f2af2);
+  d->a0 = (d->f1af1 - d->f1Mf2.act(d->f2af2) + d->f1vf1.cross(d->f1vf2)).toVector();
+}
+
+template <typename Scalar>
+void ContactModel6DLoopTpl<Scalar>::calcDiff(
+    const boost::shared_ptr<ContactDataAbstract>& data,
+    const Eigen::Ref<const VectorXs>&) {
+  Data* d = static_cast<Data*>(data.get());
+  if(gains_[0] != 0. || gains_[1] != 0.){
+    throw_pretty("Baumgarte stabilization is not implemented yet");
+  }
+  
+  const std::size_t nv = state_->get_nv();
+  pinocchio::getJointAccelerationDerivatives(
+      *state_->get_pinocchio().get(), *d->pinocchio, joint1_id_, pinocchio::LOCAL,
+      d->v1_partial_dq, d->a1_partial_dq, d->a1_partial_dv, d->a1_partial_da);
+  pinocchio::getJointAccelerationDerivatives(
+      *state_->get_pinocchio().get(), *d->pinocchio, joint2_id_, pinocchio::LOCAL,
+      d->v2_partial_dq, d->a2_partial_dq, d->a2_partial_dv, d->a2_partial_da);
+
+  d->da0_dq_t1 = joint1_placement_.toActionMatrixInverse() * d->a1_partial_dq; //
+  d->da0_dq_t2 = (d->f1af2.toActionMatrix() * (d->f1Jf1 - d->f1Xf2 * d->f2Jf2) + d->f1Xf2 * (joint2_placement_.toActionMatrixInverse()*d->a2_partial_dq)); //
+  d->da0_dq_t3 = - d->f1vf2.toActionMatrix() * (joint1_placement_.toActionMatrixInverse() * d->v1_partial_dq) // part 1
+                + d->f1vf1.toActionMatrix() * d->f1vf2.toActionMatrix() * (d->f1Jf1 - d->f1Xf2 * d->f2Jf2)            // part 2
+                + d->f1vf1.toActionMatrix() * d->f1Xf2 * (joint2_placement_.toActionMatrixInverse() * d->v2_partial_dq);     // part 3
+
+
+  d->da0_dx.leftCols(nv) = d->da0_dq_t1 - d->da0_dq_t2 + d->da0_dq_t3; // This should be da0_dq
+  d->da0_dx.rightCols(nv) = joint1_placement_.toActionMatrixInverse() * d->a1_partial_dv
+                            - d->f1Xf2 * (joint1_placement_.toActionMatrixInverse() * d->a2_partial_dv) 
+                            - d->f1vf2.toActionMatrix() * d->f1Jf1
+                            + d->f1vf1.toActionMatrix() * d->f1Xf2 * d->f2Jf2; // This should be da0_dv
+}
+
+template <typename Scalar>
+void ContactModel6DLoopTpl<Scalar>::updateForce(
+    const boost::shared_ptr<ContactDataAbstract>& data, const VectorXs& force) {
+  if (force.size() != 6) {
+    throw_pretty("Invalid argument: "
+                 << "lambda has wrong dimension (it should be 6)");
+  }
+}
+
+template <typename Scalar>
+boost::shared_ptr<ContactDataAbstractTpl<Scalar> >
+ContactModel6DLoopTpl<Scalar>::createData(pinocchio::DataTpl<Scalar>* const data) {
+  return boost::allocate_shared<Data>(Eigen::aligned_allocator<Data>(), this,
+                                      data);
+}
+
+template <typename Scalar>
+void ContactModel6DLoopTpl<Scalar>::print(std::ostream& os) const {
+  os << "ContactModel6D {frame=" << state_->get_pinocchio()->frames[id_].name
+     << ", type=" << type_ << "}";
+}
+
+
+template <typename Scalar>
+const typename MathBaseTpl<Scalar>::Vector2s&
+ContactModel6DLoopTpl<Scalar>::get_gains() const {
+  return gains_;
+}
+
+}  // namespace crocoddyl
